86a009a7c95c34bc0199292d434a5de29a98c502
SashaBausheva <abausheva@gmail.com>
2019-06-11 14:14:24 -0400

    Remove Location column from Events table
    Removed the location column (string) from the Events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_181056) do
+ActiveRecord::Schema.define(version: 2019_06_11_181333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2019_06_11_181056) do
     t.string "description"
     t.date "date"
     t.time "time"
-    t.string "location"
     t.string "organizer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
