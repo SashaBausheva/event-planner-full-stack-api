d6a433634475546558628ea1c79f9a6bea5572c2
SashaBausheva <abausheva@gmail.com>
2019-06-11 14:16:22 -0400

    Add column Location (references) to Events
    Added the locations column to the events table (references)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_181333) do
+ActiveRecord::Schema.define(version: 2019_06_11_181545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_06_11_181333) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "location_id"
+    t.index ["location_id"], name: "index_events_on_location_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -52,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_06_11_181333) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "events", "locations"
   add_foreign_key "events", "users"
   add_foreign_key "examples", "users"
 end
